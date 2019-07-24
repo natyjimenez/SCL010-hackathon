011494f405ca746a614080769eb47dbc725de22f
natyjimenez <51416113+natyjimenez@users.noreply.github.com>
2019-07-23 21:58:11 -0400

    first commit

--- src/main.js
+++ src/main.js
@@ -1,16 +1,1 @@
-window.handlingDOM = {
-    catchingSearchTerm : () =>{
-        let searchTerm = document.getElementById('searchInputBox').value;
-        return searchTerm;
-    },
 
-    catchingIdToSearch : () =>{
-        let searchTerm = document.getElementById('searchIdBox').value;
-        return searchTerm;
-    },
-
-    
-
-};
-
-window.handlingDOM = handlingDOM;
